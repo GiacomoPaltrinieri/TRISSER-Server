06d20faee4f454560ed990daa0439bdc532e7251
giacomopaltrinieri <giaco.paltri@gmail.com>
2022-02-21 17:21:57 +0100

    added single room, not tested since at the library with a computer without windows

---
+++ src/main/java/org/lauchproject/SingleRoom.java
@@ -0,0 +1,81 @@
+package org.lauchproject;
+
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SingleRoom {
+
+    /** Attributes **/
+    private int roomNumber; // mail1_mail2/38 -> roomNumber=38
+    private JSONObject players; // players and time remaining
+    private ArrayList<Integer> moves = new ArrayList<>(); // list of moves that have been made
+    private String playerToMove; //name of the player that has to move
+    private String winner; // the name of the winning bot
+    private String temp;
+    public static ArrayList<String> winningMoves = new ArrayList<>(Arrays.asList("147", "258", "369", "123", "456", "789", "159", "357"));
+
+    /** Constructor **/
+    public SingleRoom(int roomNumber, JSONObject players, ArrayList<Integer> moves, String playerToMove) {
+        this.roomNumber = roomNumber;
+        this.players = players;
+        this.playerToMove = playerToMove;
+    }
+
+    public void setMoves(int move, String user) {
+        if (user.equals(playerToMove)){
+            // user turn to move
+            if (moves.contains(move)){
+                System.out.println("move already done");
+            }else if(move > 9 || move < 1){
+                System.out.println("invalid move");
+            } else if(moves.size()<=5){
+                System.out.println("numero di mosse insufficienti per vincere, mossa valida");
+            }else{
+                moves.add(move);
+                changePlayerToMove();
+                if (isWinning(move)){
+
+                }
+            }
+        }
+    }
+
+    private boolean isWinning(int move) {
+        int oddOrNot = 0;
+        ArrayList<Integer> playerMoves = new ArrayList<>();
+        if (moves.size()%2 == 0)
+            oddOrNot = 1; // le mosse fatte da questo utente sono quelle nelle caselle pari
+
+        if (oddOrNot == 1){
+            for (int i = 0; i < moves.size() - 1; i+=2)
+                playerMoves.add(moves.get(i));
+        }
+        else
+            for (int i = 1; i < moves.size() - 1; i+=2)
+                playerMoves.add(moves.get(i));
+        Collections.sort(playerMoves);
+        System.out.println(playerMoves);
+        temp="";
+        for (int i = 0; i < playerMoves.size(); i++)
+            temp = temp + playerMoves.get(i);
+
+        for (int i = 0; i < winningMoves.size(); i++)
+            if (temp.equals(winningMoves.get(i)))
+                return true;
+        return false;
+    }
+
+    private void changePlayerToMove() {
+        players.forEach((k,v) -> {
+            if (!playerToMove.equals(k))
+                temp = (String) k;
+        });
+        playerToMove = temp;
+    }
+
+
+}
--- src/main/java/org/lauchproject/gameInstance.java
+++ src/main/java/org/lauchproject/gameInstance.java
@@ -7,42 +7,48 @@ import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 import static java.io.File.separator;
-
+/** This class describes every single room, it's players and everything related to all the games that have to played in that  **/
 public class gameInstance {
     final int getTOPIC=0;
     final int getINSTANCE=1;
     final int getUSER=2;
     /** Attributes **/
     private String topic;
-    private JSONObject players; // players involved in this room and time
-    private JSONArray  rooms; // all the rooms (room_number, ArrayList moves, userMove)
+    private String[] players = new String[2]; // players involved in this room
+    private ArrayList<SingleRoom>  rooms; // all the rooms (room_number, ArrayList moves, userMove)
     private String[] whoHasToMove; // list of who has to make a move
     private String p1,p2;
 
     /** Methods **/
 
     /** Constructor **/
-    public gameInstance(JSONObject players, JSONArray rooms, String topic) {
+    public gameInstance(String[] players, JSONArray rooms, String topic) {
         this.players = players;
         this.rooms = rooms;
-        this.topic = subStringTopic("/", topic, getTOPIC);
+        this.topic = subStringTopic("/", getTOPIC);
 
-        p1 = subStringTopic("_", this.topic, 0);
-        p2 = subStringTopic("_", this.topic, 1);
+        players[0] = subStringTopic("_", 0);
+        players[1] = subStringTopic("_", 1);
 
         whoHasToMove = new String[rooms.size()]; // if the room number is divisible by 2 p1 starts, is the room number is odd p2 starts
         for (int i = 0; i < rooms.size(); i++){
             if (i % 2 == 0)
-                whoHasToMove[i] = p1;
+                whoHasToMove[i] = players[0];
             else
-                whoHasToMove[i] = p2;
+                whoHasToMove[i] = players[1];
         }
     }
 
-    public void validMove()
+    public void validMove(){}
 
-    public void moveValidation (int move, String topic){
-        String user = subStringTopic("/", topic,getUSER);
+    public void moveValidation (int move){
+        String user = subStringTopic("/", getUSER);
+        int instance = Integer.parseInt(subStringTopic("/", getINSTANCE));
+        if (user.equals(whoHasToMove[instance])){
+            //User turn, correct
+        }else{
+            //not his turn, incorrect
+        }
     }
 //    /** Given a specific topic this function returns the user that has sent the message **/
 //    public String getUser(String topic){
@@ -53,7 +59,7 @@ public class gameInstance {
 //    }
 
     /** subtracts data from the topic **/
-    public String subStringTopic(String splitChars, String topic, int index){
+    public String subStringTopic(String splitChars, int index){
         //index = getTOPIC || (0) -> topic
         //index = getINSTANCE || (1) -> instance
         //index = getUSER || (2) -> user
