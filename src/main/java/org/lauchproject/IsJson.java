fd97c71e3cb354f00ff9b331e70341570d7614eb
abdul-00 <abdullah.ali@einaudicorreggio.it>
2022-03-01 19:27:05 +0100

    just removed the useless code and some tests

--- src/main/java/org/lauchproject/IsJson.java
+++ src/main/java/org/lauchproject/IsJson.java
@@ -1,6 +1,9 @@
 package org.lauchproject;
 
 import com.google.gson.Gson;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.simple.JSONObject;
+import java.nio.charset.StandardCharsets;
 
 public final class IsJson {
 
@@ -12,7 +15,7 @@ public final class IsJson {
 
         String fist= "" +jsonInString.charAt(0);
         String last= ""+jsonInString.charAt(jsonInString.length()-1);
-        System.out.println(fist+last);
+        //System.out.println(fist+last);
         /** Return true Only for json arrays and objects **/
         if(fist.equals("{") && last.equals("}") || fist.equals("[") && last.equals("]")){
             try {
@@ -26,4 +29,13 @@ public final class IsJson {
         }
     }
 
+    //For Testing
+    /*public static void main(String[] args){
+        JSONObject json=new JSONObject();
+        json.put("trisser.bot2@gmail.com","true");
+
+        MqttMessage message=new MqttMessage(json.toString().getBytes());
+        System.out.println("MQTTMESSAGE-->"+message);
+        System.out.println(isJSONValid(message.toString()));
+    }*/
 }
